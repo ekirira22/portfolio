3b328f45647d025782dd33e997f0a820d17e4c51
Eric Maranga <ekirira22@gmail.com>
2023-12-11 20:42:27 +0300

    added blog button for future blog

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-const contact = document.getElementById("contactme");
+const contact = document.getElementById("contact");
 
 //button click event listener -> pop up alert
 contact.addEventListener('click', () => {
     alert("Call me @ +254719405599");
 });
+
+const blog = document.getElementById("myblog");
+
+//button click event for myblog
+blog.addEventListener('click', () => {
+    alert("Coming soon");
+})
 
 const anchorr = document.querySelector('a');
 //change anchor tag color when hovered
